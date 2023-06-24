InterLinked1/wssmail

commit 02b8e7c6ebda706b47ec08af1515799e9df61f22

FETCH: Fix messaging pseudo formatting.
This fixes overly broad regex matches for
detecting *bold*, /italics/, and _underline_
in plain text messages.

We now require a space before the special
pseudo formatting character, to avoid matching
strings such as URLs with slashes anywhere
after the protocol, for instance. This also
fixes the case of these characters being used
casually in a message, when not intended as
formatting hints.

frontend/webmail.js
@@ -779,12 +779,12 @@ function setQuota(total, used) {
 function formatPT(body) {
 	/* pseudo formatting for plain text: bold, italics, underline, and hyperlinks */
 	body = escapeHTML(body); /* First, make sure plain text won't get turned into HTML */
-	/* XXX BUGBUG TODO Don't mess with these when they appear in links or are not the start/end of a word */
 
 	/* Do italics first since / appears in HTML tags (which we'll be adding to the body) */
-	body = body.replace(/\/(\S[^\/]+\S)\//g, "<i>$1</i>");
-	body = body.replace(/\*(\S[^\*]+\S)\*/g, "<b>$1</b>");
-	body = body.replace(/\_(\S[^\_]+\S)\_/g, "<u>$1</u>");
+	/* Require whitespace before match (\s), to avoid matching URLs with a request URI component */
+	body = body.replace(/\s\/(\S[^\/]+\S)\//g, "<i>$1</i>");
+	body = body.replace(/\s\*(\S[^\*]+\S)\*/g, "<b>$1</b>");
+	body = body.replace(/\s\_(\S[^\_]+\S)\_/g, "<u>$1</u>");
 
 	/* Make links hyperlinks */
 	var urlRegex = /(https?:\/\/[^\s]+)/g;

commit 453dbc2353888b3452660d0e2909ff3009f704a9

viewer: Fix plain text formatting.
Fix regression introduced in
02b8e7c6ebda706b47ec08af1515799e9df61f22
since there is now a space that has to
match for the regex, so we need to add
a space in the result.

Also, we now include the formatting characters
themselves so taht the original content
is still there, similar to what Thunderbird
clients do.

frontend/webmail.js
@@ -843,9 +843,9 @@ function formatPT(body) {
 
 	/* Do italics first since / appears in HTML tags (which we'll be adding to the body) */
 	/* Require whitespace before match (\s), to avoid matching URLs with a request URI component */
-	body = body.replace(/\s\/(\S[^\/]+\S)\//g, "<i>$1</i>");
-	body = body.replace(/\s\*(\S[^\*]+\S)\*/g, "<b>$1</b>");
-	body = body.replace(/\s\_(\S[^\_]+\S)\_/g, "<u>$1</u>");
+	body = body.replace(/\s\/(\S[^\/]+\S)\//g, " <i>/$1/</i>");
+	body = body.replace(/\s\*(\S[^\*]+\S)\*/g, " <b>*$1*</b>");
+	body = body.replace(/\s\_(\S[^\_]+\S)\_/g, " <u>_$1_</u>");
 
 	/* Make links hyperlinks */
 	var urlRegex = /(https?:\/\/[^\s]+)/g;